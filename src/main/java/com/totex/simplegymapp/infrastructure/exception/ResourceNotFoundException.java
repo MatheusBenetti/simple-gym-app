d931f5389e36e3c0dafe659b53688950a31dcbbd
MatheusBenetti <mbbenetti1@gmail.com>
2025-06-19 15:54:52 -0300

    Create new exception

---
+++ src/main/java/com/totex/simplegymapp/infrastructure/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.totex.simplegymapp.infrastructure.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
